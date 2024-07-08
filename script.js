2159d4b3e02d0cc57332dd28f13f09dd317af238
Tucker McKee <tuckermckee@Tuckers-MacBook-Pro.local>
2024-07-08 14:48:40 -0500

    cleaned up css, removed listDisplay in js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
-const listDisplay = document.querySelector('ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
@@ -11,7 +10,7 @@ function search(str) {
 
 function searchHandler(e) {
 	if (e.key === 'Backspace' && input.value === ''){
-		listDisplay.style.borderStyle = 'none';
+		suggestions.style.borderStyle = 'none';
 		suggestions.innerHTML = '';
 		return
 	}
@@ -19,9 +18,9 @@ function searchHandler(e) {
 		suggestions.innerHTML = '';
 		showSuggestions(search(e.target.value));
 		if (!search(e.target.value)[0]){
-			listDisplay.style.borderStyle = 'none';
+			suggestions.style.borderStyle = 'none';
 		} else {
-			listDisplay.style.borderStyle = 'solid';
+			suggestions.style.borderStyle = 'solid';
 		}
 	}
 }
@@ -37,7 +36,7 @@ function showSuggestions(results) {
 function useSuggestion(e) {
 	input.value = e.target.innerText;
 	suggestions.innerHTML = '';
-	listDisplay.style.borderStyle = 'none';
+	suggestions.style.borderStyle = 'none';
 }
 
 input.addEventListener('keyup', searchHandler);
